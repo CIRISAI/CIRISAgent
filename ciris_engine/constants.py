a11a27e8ed01b102ffe6fe21b562f41bdbf7d7de
Eric Moore <mooreericnyc@gmail.com>
2026-01-20 15:16:00 -0600

    chore: bump version to 1.8.12-stable

--- ciris_engine/constants.py
+++ ciris_engine/constants.py
@@ -3,11 +3,11 @@
 from pathlib import Path
 
 # Version information
-CIRIS_VERSION = "1.8.11-stable"
+CIRIS_VERSION = "1.8.12-stable"
 COVENANT_VERSION = "1.2-Beta"
 CIRIS_VERSION_MAJOR = 1
 CIRIS_VERSION_MINOR = 8
-CIRIS_VERSION_PATCH = 11
+CIRIS_VERSION_PATCH = 12
 CIRIS_VERSION_BUILD = 0
 CIRIS_VERSION_STAGE = "stable"
 CIRIS_CODENAME = "Context Engineering"  # Codename for this release
